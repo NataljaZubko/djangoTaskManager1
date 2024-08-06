ebd9e851310c6b7bf599534d0475a8f5e0dc362c
NataljaZubko <168180118+NataljaZubko@users.noreply.github.com>
2024-08-06 21:52:54 +0200

    Add in models file
    Добавлены строковое представление и метаданные к моделям

--- myapp/models.py
+++ myapp/models.py
@@ -1,39 +1,65 @@
 from django.db import models
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        db_table = 'task_manager_category'
+        verbose_name = 'Category'
+        unique_together = ['name']
+
+
 class Task(models.Model):
     STATUS_CHOICES = [
-        ('New', 'New'),
-        ('In progress', 'In progress'),
-        ('Pending', 'Pending'),
-        ('Blocked', 'Blocked'),
-        ('Done', 'Done'),
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done')
     ]
 
-    title = models.CharField(max_length=200, unique_for_date='created_at')
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    categories = models.ManyToManyField(Category)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='New')
+    categories = models.ManyToManyField(Category, related_name='tasks')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'task_manager_task'
+        verbose_name = 'Task'
+        ordering = ['-created_at']
+        unique_together = ['title', 'deadline']
+
 
 class SubTask(models.Model):
-    STATUS_CHOICES = Task.STATUS_CHOICES
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done')
+    ]
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='New')
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'task_manager_subtask'
+        verbose_name = 'SubTask'
+        ordering = ['-created_at']
+        unique_together = ['title']
