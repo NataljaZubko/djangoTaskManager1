b873eff68e00e06adcc403bed1f16c52b63a323f
NataljaZubko <168180118+NataljaZubko@users.noreply.github.com>
2024-08-06 21:55:53 +0200

    Add in admin file
    Настроено отображение моделей в админ файле

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,38 +1,25 @@
 from django.contrib import admin
 from .models import Task, SubTask, Category
 
+
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'created_at')
-    list_filter = ('status',)
-    search_fields = ('title', 'description')
-    fieldsets = (
-        (None, {
-            'fields': ('title', 'description', 'categories')
-        }),
-        ('Task Details', {
-            'fields': ('status', 'deadline'),
-            'description': 'Статус и дедлайн задачи'
-        }),
-    )
+    search_fields = ('title',)
+    list_filter = ('status', 'categories')
+    ordering = ('-created_at',)
 
+
+@admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'task', 'status', 'deadline', 'created_at')
-    list_filter = ('status', 'task__title')  # Filter by task title
-    search_fields = ('title', 'description')
-    fieldsets = (
-        (None, {
-            'fields': ('title', 'description', 'task')
-        }),
-        ('SubTask Details', {
-            'fields': ('status', 'deadline'),
-            'description': 'Статус и дедлайн подзадачи'
-        }),
-    )
+    search_fields = ('title', 'task__title')
+    list_filter = ('status', 'task')
+    ordering = ('-created_at',)
 
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
-
-admin.site.register(Task, TaskAdmin)
-admin.site.register(SubTask, SubTaskAdmin)
-admin.site.register(Category, CategoryAdmin)
+    ordering = ('name',)
